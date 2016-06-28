b54a96a3d986ef0e5bdf8c073a8e73fb2e3a2c96
Eric Lagergren <ericscottlagergren@gmail.com>
2016-06-28 13:28:39 -0700

    slight arith_test updates

--- internal/arith/arith_test.go
+++ internal/arith/arith_test.go
@@ -7,11 +7,11 @@ func TestCLZ(t *testing.T) {
 		t.Errorf("got CLZ(0) = %d; want 0", n)
 	}
 
-	x := int64(1<<(_W-1) - 1)
+	x := int64((_B >> 1) / 2)
 	for i := int64(0); i > 0; i++ {
 		n := CLZ(x)
 		if int64(n) != i {
-			t.Errorf("got CLZ(%#x) = %d; want %d", x, n, i%_W)
+			t.Errorf("got CLZ(%#x) = %d; want %d", x, n, i)
 		}
 		x >>= 1
 	}
@@ -26,8 +26,18 @@ func TestCTZ(t *testing.T) {
 	for i := int64(0); i < _W; i++ {
 		n := CTZ(x)
 		if int64(n) != i {
-			t.Errorf("got CTZ(%#x) = %d; want %d", x, n, i%_W)
+			t.Errorf("got CTZ(%#x) = %d; want %d", x, n, i)
 		}
 		x <<= 1
+	}
+}
+
+func TestBitLen(t *testing.T) {
+	for i := 0; i <= _W; i++ {
+		x := uint64(1) << uint64(i-1)
+		n := BitLen(int64(x))
+		if n != i {
+			t.Errorf("got BitLen(%#x) = %d; want %d", x, n, i)
+		}
 	}
 }
