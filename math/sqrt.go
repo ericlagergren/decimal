bd3847556028da8e3dd16d83e721b1c7cafacbf3
Eric Lagergren <ericscottlagergren@gmail.com>
2016-07-13 15:14:28 -0700

    math: add Hypot

--- math/sqrt.go
+++ math/sqrt.go
@@ -8,6 +8,24 @@ import (
 	"github.com/EricLagergren/decimal/internal/arith"
 )
 
+// Hypot sets z to Sqrt(p*p + q*q) and returns z.
+func Hypot(z, p, q *decimal.Big) *decimal.Big {
+	p0 := new(decimal.Big).Set(p)
+	q0 := new(decimal.Big).Set(q)
+	if p0.Sign() <= 0 {
+		p0.Neg(p0)
+	}
+	if q0.Sign() <= 0 {
+		q0.Neg(q0)
+	}
+	if p0.Sign() == 0 {
+		return z.SetMantScale(0, 0)
+	}
+	p0.Mul(p0, p0)
+	q0.Mul(q0, q0)
+	return Sqrt(z, p0.Add(p0, q0))
+}
+
 // Sqrt sets z to the square root of x and returns z.
 // The precision of Sqrt is determined by z's Context.
 // Sqrt will panic on negative values since decimal.Big cannot
