167c205705ca7e5d0c5f0019b6d64a9610988381
Eric Lagergren <ericscottlagergren@gmail.com>
2017-09-27 17:20:38 -0700

    sql: add support for postgres' DECIMAL type
    Former-commit-id: 6f839cac07e7b00ef667254f2cc0dc1014bbbead [formerly 4713c794655dd67acb07d337d77e734e571561c2]
    Former-commit-id: da837c193a66180095944796e83aa17c30a4b277

---
+++ sql/postgres/decimal.go
@@ -0,0 +1,73 @@
+package postgres
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+
+	"github.com/ericlagergren/decimal"
+)
+
+const (
+	MaxIntegralDigits   = 131072
+	MaxFractionalDigits = 16383
+)
+
+// LengthError is returned from Decimal.Value when either its integral (digits
+// before the decimal point) or fractional (digits after the decimal point)
+// parts are too long for PostgresSQL.
+type LengthError struct {
+	Part string // "integral" or "fractional"
+	N    int    // length of invalid part
+	max  int
+}
+
+func (e LengthError) Error() string {
+	return fmt.Sprintf("%s (%d digits) is too long (%d max)", e.Part, e.N, e.max)
+}
+
+// Decimal is a PostgreSQL DECIMAL.
+type Decimal struct {
+	*decimal.Big
+	Round bool // round if the decimal exceeds the bounds for DECIMAL
+}
+
+// Value implements driver.Valuer.
+func (d *Decimal) Value() (driver.Value, error) {
+	if d.IsNaN(true) || d.IsNaN(false) {
+		return "NaN", nil
+	}
+	if d.IsInf(0) {
+		return nil, errors.New("Decimal.Value: DECIMAL does not accept Infinities")
+	}
+
+	dl := d.Precision()  // length of d
+	sl := int(d.Scale()) // length of fractional part
+
+	if il := dl - sl; il > MaxIntegralDigits {
+		if !d.Round {
+			return nil, &LengthError{Part: "integral", N: il, max: MaxIntegralDigits}
+		}
+		// Rounding down the integral part automatically chops off the fractional
+		// part.
+		return d.Big.Round(MaxIntegralDigits).String(), nil
+	}
+	if sl > MaxFractionalDigits {
+		if !d.Round {
+			return nil, &LengthError{Part: "fractional", N: sl, max: MaxFractionalDigits}
+		}
+		d.Big.Round(int32(dl - (sl - MaxFractionalDigits)))
+	}
+	return d.String(), nil
+}
+
+// Scan implements sql.Scanner.
+func (d *Decimal) Scan(val interface{}) error {
+	switch v := val.(type) {
+	case string:
+		d.SetString(v)
+	default:
+		return fmt.Errorf("Decimal.Scan: unknown value: %#v", val)
+	}
+	return nil
+}
---
+++ sql/postgres/decimal_test.go
@@ -0,0 +1,73 @@
+package postgres
+
+import (
+	"fmt"
+	"math/big"
+	"math/rand"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/ericlagergren/decimal"
+)
+
+var (
+	maxInt, _  = new(big.Int).SetString(strings.Repeat("9", MaxIntegralDigits*2), 10)
+	maxFrac, _ = new(big.Int).SetString(strings.Repeat("9", MaxFractionalDigits*2), 10)
+	r          = rand.New(rand.NewSource(1))
+)
+
+func randInt(n *big.Int) string { return new(big.Int).Rand(r, n).String() }
+
+func TestDecimal_Value(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		ip := randInt(maxInt)
+		fp := randInt(maxFrac)
+
+		dec, ok := new(decimal.Big).SetString(fmt.Sprintf("%s.%s", ip, fp))
+		if !ok {
+			t.Fatal(dec.Err())
+		}
+		d := Decimal{Big: dec, Round: i%2 == 0}
+
+		v, err := d.Value()
+		if err != nil {
+			if d.Round {
+				t.Fatalf("#%d: err == %#v when d.Round == true", i, err)
+			}
+			switch e := err.(*LengthError); e.Part {
+			case "integral":
+				if len(ip) != e.N {
+					t.Fatalf("#%d: reported int len of %d, got %d", i, e.N, len(ip))
+				}
+			case "fractional":
+				if len(fp) != e.N {
+					t.Fatalf("#%d: reported frac len of %d, got %d", i, e.N, len(fp))
+				}
+			default:
+				t.Fatalf("#%d: bad part: %q", i, e.Part)
+			}
+			continue
+		}
+
+		vs := v.(string)
+
+		switch parts := strings.Split(vs, "."); len(parts) {
+		case 2:
+			var e int
+			if i := strings.LastIndexAny(vs, "eE"); i > 0 {
+				ev, _ := strconv.ParseInt(vs[i+1:], 10, 32)
+				e -= int(ev)
+				vs = vs[:i]
+			}
+			if len(parts[1])+e > MaxFractionalDigits {
+				t.Fatalf("%#d: frac part too long: %d", i, len(parts[1]))
+			}
+			fallthrough
+		case 1:
+			if len(parts[0]) > MaxIntegralDigits {
+				t.Fatalf("#%d: int part too long: %d", i, len(parts[0]))
+			}
+		}
+	}
+}
