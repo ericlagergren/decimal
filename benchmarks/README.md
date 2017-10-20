2254972d41238f3397f7e70fead18a68981422d9
Eric Lagergren <ericscottlagergren@gmail.com>
2017-10-20 15:08:00 -0700

    benchmarks: add shopspring
    Fixes: #40
    
    Former-commit-id: cd6a7d7ae561fe9f86797b48a47394f1b9c58a08 [formerly 1059aa1a0b13b6c8f98f01516fff73565fe34083]
    Former-commit-id: 2d7d21eb6fa8683ffbda79d1947d80d0c55ac195

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -1,23 +1,25 @@
 ## Benchmarks for various decimal programs.
 
-- Times are measured in seconds unless otherwise noted
+- Times are measured in seconds, unless otherwise noted
 - Measured on a MacBook Pro, 2.9 GHz Intel Core i5, 8 GB 2133 MHz LPDDR3
-- Some benchmarks are adapted from
-www.bytereef.org/mpdecimal/benchmarks.html
+- Some benchmarks are adapted from www.bytereef.org/mpdecimal/benchmarks.html
 
 ## Pi
-|    Program (version)                  | 9 digits |  19 digits  | 38 digits | 100 digits | average |
-|-----------------------------|----------|-------------|-----------|------------|---------|
-| BigDecimal<sup>[4]</sup> (Java 1.8, warm) | 0.049    | 0.19        | 0.6       | 3.29       | 1.05    |
-| **decimal**<sup>[1]</sup> (Go 1.9)            | 0.031    | 0.51        | 1.22      | 4.15       | 1.47    |
-| decimal<sup>[5]</sup> (Python 3.6.2)      | 0.27     | 0.58        | 1.32      | 4.52       | 1.67    |
-| BigDecimal (Java 1.8)       | 0.29     | 0.96        | 1.79      | 3.99       | 1.76    |
-| apd<sup>[2]</sup> (Go 1.9)                | 0.52     | 2.21        | 8.98      | 68.43      | 20.03   |
-| decimal<sup>[6]</sup> (Python 2.7.10)     | 12.93    | 28.91       | 64.96     | 192.58     | 74.84   |
-| float64 (Go 1.9)            | 0.057    | -           | -         | -          | -       |
-| double (C LLVM 9.0.0 -O3)   | 0.057    | -           | -         | -          | -       |
-| float (Python 2.7.10)       | 0.59     | -           | -         | -          | -       |
-| dnum<sup>[3]</sup> (Go 1.9)               | 0.95     | -           | -         | -          | -       |
+
+|    Program (version)                      | 9 digits |  19 digits  | 38 digits | 100 digits | average |
+|-------------------------------------------|----------|-------------|-----------|------------|---------|
+| [JDK BigDecimal][4] (Java 1.8, warm)           | 0.049    | 0.19        | 0.6       | 3.29       | 1.05    |
+| [ericlagergren/decimal][1] (Go 1.9, mode Go)   | 0.034    | 0.40        | 1.00      | 3.57       | 1.25    |
+| [Python decimal][5] (Python 3.6.2)             | 0.27     | 0.58        | 1.32      | 4.52       | 1.67    |
+| [ericlagergren/decimal][1] (Go 1.9, mode GDA)  | 0.048    | 0.55        | 1.46      | 4.91       | 1.74    |
+| [JDK BigDecimal][4] (Java 1.8)                 | 0.29     | 0.96        | 1.79      | 3.99       | 1.76    |
+| [shopspring/decimal][7] decimal (Go 1.9)       | 0.38     | 0.94        | 1.95      | 5.26       | 2.13    |
+| [cockroachdb/apd][2] (Go 1.9)                  | 0.52     | 2.14        | 9.01      | 71.62      | 20.81   |
+| [Python decimal][6] (Python 2.7.10)            | 12.93    | 28.91       | 64.96     | 192.58     | 74.84   |
+| float64 (Go 1.9)                          | 0.057    | -           | -         | -          | -       |
+| double (C LLVM 9.0.0 -O3)                 | 0.057    | -           | -         | -          | -       |
+| [apmckinlay/dnum][3] (Go 1.9)                  | 0.091    | -           | -         | -          | -       |
+| float (Python 2.7.10)                     | 0.59     | -           | -         | -          | -       |
 
 [1]: https://github.com/ericlagergren/decimal
 [2]: https://github.com/cockroachdb/apd
@@ -25,3 +27,4 @@ www.bytereef.org/mpdecimal/benchmarks.html
 [4]: https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html
 [5]: https://docs.python.org/3.6/library/decimal.html
 [6]: https://docs.python.org/2/library/decimal.html
+[7]: https://github.com/shopspring/decimal
--- benchmarks/pi_test.go
+++ benchmarks/pi_test.go
@@ -6,22 +6,49 @@ import (
 	"github.com/apmckinlay/gsuneido/util/dnum"
 	"github.com/cockroachdb/apd"
 	"github.com/ericlagergren/decimal"
+	ssdec "github.com/shopspring/decimal"
 )
 
-func newd(c int64, m int32, p int32) *decimal.Big {
+func newd(c int64, m int32, p int32, mode decimal.OperatingMode) *decimal.Big {
 	d := decimal.New(c, m)
 	d.Context.SetPrecision(p)
+	d.Context.OperatingMode = mode
 	return d
 }
 
 var (
-	eight         = decimal.New(8, 0)
-	thirtyTwo     = decimal.New(32, 0)
-	apdEight      = apd.New(8, 0)
-	apdThirtyTwo  = apd.New(32, 0)
-	dnumEight     = dnum.NewDnum(false, 8, 0)
-	dnumThirtyTwo = dnum.NewDnum(false, 32, 0)
+	eight          = decimal.New(8, 0)
+	thirtyTwo      = decimal.New(32, 0)
+	apdEight       = apd.New(8, 0)
+	apdThirtyTwo   = apd.New(32, 0)
+	dnumEight      = dnum.NewDnum(false, 8, 0)
+	dnumThirtyTwo  = dnum.NewDnum(false, 32, 0)
+	ssdecEight     = ssdec.New(8, 0)
+	ssdecThirtyTwo = ssdec.New(32, 0)
 )
+
+func calcPi_shopSpring(prec int32) ssdec.Decimal {
+	var (
+		lasts = ssdec.New(0, 0)
+		t     = ssdec.New(3, 0)
+		s     = ssdec.New(3, 0)
+		n     = ssdec.New(1, 0)
+		na    = ssdec.New(0, 0)
+		d     = ssdec.New(0, 0)
+		da    = ssdec.New(24, 0)
+	)
+	for s.Cmp(lasts) != 0 {
+		lasts = s
+		n = n.Add(na)
+		na = na.Add(ssdecEight)
+		d = d.Add(da)
+		da = da.Add(ssdecThirtyTwo)
+		t = t.Mul(n)
+		t = t.DivRound(d, prec)
+		s = s.Add(t)
+	}
+	return s
+}
 
 func calcPi_dnum() dnum.Dnum {
 	var (
@@ -68,15 +95,15 @@ func calcPi_float() float64 {
 	return s
 }
 
-func calcPi(prec int32) *decimal.Big {
+func calcPi(prec int32, mode decimal.OperatingMode) *decimal.Big {
 	var (
-		lasts = newd(0, 0, prec)
-		t     = newd(3, 0, prec)
-		s     = newd(3, 0, prec)
-		n     = newd(1, 0, prec)
-		na    = newd(0, 0, prec)
-		d     = newd(0, 0, prec)
-		da    = newd(24, 0, prec)
+		lasts = newd(0, 0, prec, mode)
+		t     = newd(3, 0, prec, mode)
+		s     = newd(3, 0, prec, mode)
+		n     = newd(1, 0, prec, mode)
+		na    = newd(0, 0, prec, mode)
+		d     = newd(0, 0, prec, mode)
+		da    = newd(24, 0, prec, mode)
 	)
 	for s.Cmp(lasts) != 0 {
 		lasts.Set(s)
@@ -116,19 +143,20 @@ func calcPi_apd(prec uint32) *apd.Decimal {
 }
 
 var (
-	gf     float64
-	gs     *decimal.Big
-	apdgs  *apd.Decimal
-	dnumgs dnum.Dnum
+	gf      float64
+	gs      *decimal.Big
+	apdgs   *apd.Decimal
+	dnumgs  dnum.Dnum
+	ssdecgs ssdec.Decimal
 )
 
 const rounds = 10000
 
-func benchPi(b *testing.B, prec int32) {
+func benchPi(b *testing.B, prec int32, mode decimal.OperatingMode) {
 	var ls *decimal.Big
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < rounds; j++ {
-			ls = calcPi(prec)
+			ls = calcPi(prec, mode)
 		}
 	}
 	gs = ls
@@ -154,7 +182,17 @@ func BenchmarkPi_dnum(b *testing.B) {
 	dnumgs = ls
 }
 
-func BenchmarkPi_Baseline(b *testing.B) {
+func benchPi_shopspring(b *testing.B, prec int32) {
+	var ls ssdec.Decimal
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < rounds; j++ {
+			ls = calcPi_shopSpring(prec)
+		}
+	}
+	ssdecgs = ls
+}
+
+func BenchmarkPi_BaselineFloat64(b *testing.B) {
 	var lf float64
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < rounds; j++ {
@@ -164,12 +202,22 @@ func BenchmarkPi_Baseline(b *testing.B) {
 	gf = lf
 }
 
-func BenchmarkPi_9(b *testing.B)   { benchPi(b, 9) }
-func BenchmarkPi_19(b *testing.B)  { benchPi(b, 19) }
-func BenchmarkPi_38(b *testing.B)  { benchPi(b, 38) }
-func BenchmarkPi_100(b *testing.B) { benchPi(b, 100) }
+func BenchmarkPi_decimal_Go_9(b *testing.B)   { benchPi(b, 9, decimal.Go) }
+func BenchmarkPi_decimal_Go_19(b *testing.B)  { benchPi(b, 19, decimal.Go) }
+func BenchmarkPi_decimal_Go_38(b *testing.B)  { benchPi(b, 38, decimal.Go) }
+func BenchmarkPi_decimal_Go_100(b *testing.B) { benchPi(b, 100, decimal.Go) }
+
+func BenchmarkPi_decimal_GDA_9(b *testing.B)   { benchPi(b, 9, decimal.GDA) }
+func BenchmarkPi_decimal_GDA_19(b *testing.B)  { benchPi(b, 19, decimal.GDA) }
+func BenchmarkPi_decimal_GDA_38(b *testing.B)  { benchPi(b, 38, decimal.GDA) }
+func BenchmarkPi_decimal_GDA_100(b *testing.B) { benchPi(b, 100, decimal.GDA) }
 
 func BenchmarkPi_apd_9(b *testing.B)   { benchPi_apd(b, 9) }
 func BenchmarkPi_apd_19(b *testing.B)  { benchPi_apd(b, 19) }
 func BenchmarkPi_apd_38(b *testing.B)  { benchPi_apd(b, 38) }
 func BenchmarkPi_apd_100(b *testing.B) { benchPi_apd(b, 100) }
+
+func BenchmarkPi_shopspring_9(b *testing.B)   { benchPi_shopspring(b, 9) }
+func BenchmarkPi_shopspring_19(b *testing.B)  { benchPi_shopspring(b, 19) }
+func BenchmarkPi_shopspring_38(b *testing.B)  { benchPi_shopspring(b, 38) }
+func BenchmarkPi_shopspring_100(b *testing.B) { benchPi_shopspring(b, 100) }
