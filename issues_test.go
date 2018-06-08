b8cf11b1ebaae28cabc9922395f6a0276978d0a9
Timothy Ham <tham@salesforce.com>
2018-06-08 09:46:26 -0700

    Issue 99
    Address reviewer comment: move test to issues_test.go

--- issues_test.go
+++ issues_test.go
@@ -52,3 +52,17 @@ got   : %s
 `, x, y, z)
 	}
 }
+
+func TestMulTestIssue99(t *testing.T) {
+	bad := &Big{compact: 11234567890123457890, exp: -20, precision: 20}
+	ref := &Big{compact: 2, exp: -1, precision: 1}
+
+	cmp := bad.Cmp(ref)
+	if cmp != -1 {
+		t.Errorf("expected %s smaller than %s\n", bad.String(), ref.String())
+	}
+	cmp = ref.Cmp(bad)
+	if cmp != 1 {
+		t.Errorf("expected %s larger than %s\n", ref.String(), bad.String())
+	}
+}
