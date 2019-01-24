a33819c3104e36e54c050b4d23e16635cc9bf6dc
Richard Dingwall <rdingwall@gmail.com>
2019-01-24 23:29:31 +0000

    Fix pytables

--- testdata/pytables/generate.bash
+++ testdata/pytables/generate.bash
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-if [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
+if [ "${TRAVIS_EVENT_TYPE}" == "cron" ] || ! ls testdata/pytables/*.gz 1> /dev/null 2>&1; then
 	pushd testdata/pytables
 	time ./tables.py 500
 	popd
