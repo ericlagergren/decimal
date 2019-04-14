ericlagergren/decimal

commit 2cd9f5e8c5b22a6e2d4a52c22a1af6ceff3df82f

minor code cleanup, add docs

internal/arith/arith_1_11.go
@@ -16,6 +16,15 @@ func Add64(x, y uint64) (sum, carryOut uint64) {
 	return
 }
 
+func Sub64(x, y, borrow uint64) (diff, borrowOut uint64) {
+	yb := y + borrow
+	diff = x - yb
+	if diff > x || yb < y {
+		borrowOut = 1
+	}
+	return
+}
+
 func Mul64(x, y uint64) (hi, lo uint64) {
 	const mask32 = 1<<32 - 1
 	x0 := x & mask32

big.go
@@ -24,7 +24,7 @@ import (
 // the number of decimal digits after the radix. Otherwise, the number is
 // multiplied by 10 to the power of the negation of the scale. More formally,
 //
-//   Big = number × 10**-scale
+//    Big = number × 10**-scale
 //
 // with MinScale <= scale <= MaxScale. A Big may also be ±0, ±Infinity, or ±NaN
 // (either quiet or signaling). Non-NaN Big values are ordered, defined as the
@@ -46,7 +46,8 @@ type Big struct {
 
 	// exp is the negated scale, meaning
 	//
-	//   number × 10**exp = number ×  10**-scale
+	//   number × 10**exp = number × 10**-scale
+	//
 	exp int
 
 	// precision is the current precision.
@@ -419,9 +420,9 @@ func cmpabs(x, y *Big) int {
 	if arith.Safe(shift) && x.isCompact() && y.isCompact() {
 		p, _ := arith.Pow10(shift)
 		if diff < 0 {
-			return arith.AbsCmp(x.compact, y.compact, p)
+			return arith.CmpShift(x.compact, y.compact, p)
 		}
-		return -arith.AbsCmp(y.compact, x.compact, p)
+		return -arith.CmpShift(y.compact, x.compact, p)
 	}
 
 	xw, yw := x.unscaled.Bits(), y.unscaled.Bits()
@@ -1372,11 +1373,12 @@ func (x *Big) ord(abs bool) int {
 
 // Sign returns:
 //
-//	-1 if x <  0
-//	 0 if x == 0
-//	+1 if x >  0
+//    -1 if x <  0
+//     0 if x == 0
+//    +1 if x >  0
 //
-// The result is undefined if x is a NaN value.
+// No distinction is made between +0 and -0. The result is undefined if x is a
+// NaN value.
 func (x *Big) Sign() int {
 	if debug {
 		x.validate()
@@ -1391,7 +1393,7 @@ func (x *Big) Sign() int {
 	return 1
 }
 
-// Signbit returns true if x is negative, negative infinity, negative zero, or
+// Signbit reports whether x is negative, negative zero, negative infinity, or
 // negative NaN.
 func (x *Big) Signbit() bool {
 	if debug {

big_ctx.go
@@ -106,58 +106,80 @@ func (c Context) tryTinyAdd(z *Big, hi *Big, hineg form, lo *Big, loneg form) (s
 	return sign, true
 }
 
-func (c Context) addCompact(z *Big, hi uint64, hineg form, lo uint64, loneg form, shift uint64) (sign form) {
-	sign = hineg
-	if hi, ok := checked.MulPow10(hi, shift); ok {
-		if loneg == hineg {
-			if z1, z0 := arith.Add64(hi, lo); z1 == 0 {
-				z.compact = z0
-				if z0 == cst.Inflated {
-					z.unscaled.SetUint64(cst.Inflated)
-				}
-				z.precision = arith.Length(z.compact)
-			} else {
-				arith.Set(&z.unscaled, z1, z0)
-				z.precision = 20
-				z.compact = cst.Inflated
-			}
-			return sign
+// addCompact sets z to X + Y where
+//
+//    X = X0 * 10^shift
+//
+// and returns the resulting signbit.
+func (c Context) addCompact(z *Big, X0 uint64, Xsign form, Y uint64, Ysign form, shift uint64) form {
+	// Test whether X0 * 10^shift fits inside a uint64. If not, fall back to
+	// big.Ints.
+	X, ok := checked.MulPow10(X0, shift)
+	if !ok {
+		X0 := z.unscaled.SetUint64(X0)
+		X := checked.MulBigPow10(X0, X0, shift)
+		// Because hi was promoted to a big.Int, it by definition is larger than
+		// lo.
+		//
+		// Therefore, the resulting signbit is the same as hi's signbit.
+		//
+		// Furthermore, we do not need to check if the result of the operation
+		// is zero.
+		if Xsign == Ysign {
+			z.precision = arith.BigLength(arith.Add(&z.unscaled, X, Y))
+			z.compact = cst.Inflated
+		} else {
+			arith.Sub(&z.unscaled, X, Y)
+			z.norm()
 		}
+		return Xsign
+	}
 
-		if z.compact, ok = checked.Sub(hi, lo); !ok {
-			sign ^= signbit
-			z.compact = lo - hi
-		}
-
-		// "Otherwise, the sign of a zero result is 0 unless either both
-		// operands were negative or the signs of the operands were different
-		// and the rounding is round-floor."
-		if z.compact == 0 {
-			z.precision = 1
-			if (hineg&loneg == signbit) ||
-				(hineg^loneg == signbit && c.RoundingMode == ToNegativeInf) {
-				return signbit
+	// If the signs are the same, then X op Y = ℤ≠0.
+	if Ysign == Xsign {
+		if sum, c := arith.Add64(X, Y); c == 0 {
+			z.compact = sum
+			if sum == cst.Inflated {
+				z.unscaled.SetUint64(cst.Inflated)
 			}
-			return 0
+			z.precision = arith.Length(z.compact)
+		} else {
+			arith.Set(&z.unscaled, c, sum)
+			z.precision = 20
+			z.compact = cst.Inflated
 		}
+		return Xsign
+	}
+
+	sign := Xsign
+	// X + (-Y) == X - Y == -(Y - X)
+	// (-X) + Y == Y - X == -(X - Y)
+	diff, b := arith.Sub64(X, Y)
+	if b != 0 {
+		sign ^= signbit
+		diff = Y - X
+	}
+
+	if diff != 0 {
+		z.compact = diff
 		z.precision = arith.Length(z.compact)
 		return sign
 	}
 
-	{
-		hi := z.unscaled.SetUint64(hi)
-		hi = checked.MulBigPow10(hi, hi, shift)
-		if hineg == loneg {
-			z.precision = arith.BigLength(arith.Add(&z.unscaled, hi, lo))
-			z.compact = cst.Inflated
-		} else {
-			arith.Sub(&z.unscaled, hi, lo)
-			z.norm()
-		}
-		// hi had to be promoted to a big.Int, so by definition it'll be larger
-		// than lo. Therefore, we do not need to negate neg in the above else
-		// case, nor do we need to check to see if the result == 0.
+	// On a zero result:
+	//
+	//    Otherwise, the sign of a zero result is 0 unless either both operands
+	//    were negative or the signs of the operands were different and the
+	//    rounding is round-floor.
+	//
+	// - http://speleotrove.com/decimal/daops.html#refaddsub
+	if c.RoundingMode == ToNegativeInf {
+		sign = finite | (Xsign ^ Ysign)
+	} else {
+		sign = finite | (Xsign & Ysign)
 	}
+	z.compact = 0
+	z.precision = 1
 	return sign
 }
 

internal/arith/arith_1_12.go
@@ -7,33 +7,15 @@ import (
 	"math/bits"
 )
 
-func Add64(x, y uint64) (z1, z0 uint64) {
-	z0 = x + y
-	z1 = (x&y | (x|y)&^z0) >> 63
-	return z1, z0
+func Add64(x, y uint64) (sum, carryOut uint64) {
 	return bits.Add64(x, y, 0)
 }
 
-func Mul64(x, y uint64) (z1, z0 uint64) {
-	const mask = 0xFFFFFFFF
+func Sub64(x, y uint64) (diff, borrowOut uint64) {
+	return bits.Sub64(x, y, 0)
+}
 
-	x1 := x & mask
-	y1 := y & mask
-	t := x1 * y1
-	w3 := t & mask
-	k := t >> 32
-
-	x >>= 32
-	t = (x * y1) + k
-	k = (t & mask)
-	w1 := t >> 32
-
-	y >>= 32
-	t = (x1 * y) + k
-
-	z1 = (x * y) + w1 + (t >> 32)
-	z0 = (t << 32) + w3
-	return z1, z0
+func Mul64(x, y uint64) (hi, lo uint64) {
 	return bits.Mul64(x, y)
 }
 

internal/arith/intlen.go
@@ -37,7 +37,7 @@ func BigLength(x *big.Int) int {
 		// we can break nb into two factors: overflowCutoff and X.
 
 		// overflowCutoff / log10(2)
-		m = 4294967295
+		m = 1<<32 - 1
 		nb = (nb / overflowCutoff) + (nb % overflowCutoff)
 	}
 

commit 809c7c270edc44137656dc9dc023566f087df229

Resolve Go 1.11 compile issue introduced in 2cd9f5e8c5b2
The 1.11 API for Sub64() was accidentally copied from math/bits which doesn't
match the behavior expected by big_ctx.

internal/arith/arith_1_11.go
@@ -16,8 +16,8 @@ func Add64(x, y uint64) (sum, carryOut uint64) {
 	return
 }
 
-func Sub64(x, y, borrow uint64) (diff, borrowOut uint64) {
-	yb := y + borrow
+func Sub64(x, y uint64) (diff, borrowOut uint64) {
+	yb := y
 	diff = x - yb
 	if diff > x || yb < y {
 		borrowOut = 1